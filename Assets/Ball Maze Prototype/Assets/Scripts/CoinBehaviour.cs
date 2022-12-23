c1307912f5728de27fb3dd5019df0c1b02cdd496
CptJoshPhil <joshua.chingkaw@ciit.edu.ph>
2022-12-23 21:34:35 +0800

    Prototype Maze

---
+++ Assets/Ball Maze Prototype/Assets/Scripts/CoinBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject particlePrefab;
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private Vector3 rotateDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateDirection * rotateSpeed * Time.deltaTime);
+    }
+
+    private void OnDestroy()
+    {
+        Instantiate(particlePrefab, transform.position, Quaternion.identity);
+    }
+}
